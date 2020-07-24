d08cfdc91e44a710a7cb3cc2f1b4720718b3fce5
Alan Nguyen <tuan8101@gmail.com>
2020-07-23 23:32:57 -0600

    Create AddTransaction

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const AddTransaction = () => {
+  return (
+    <div>
+      <h3>Add new transaction</h3>
+      <form id="form">
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text" id="text" placeholder="Enter text ..." />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount
+            <br />
+            (negative - expense, positive - income)
+          </label>
+          <input type="text" id="amount" placeholder="Enter amount ..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </div>
+  );
+};
