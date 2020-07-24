7d66d90280d2db740150903770240204ccf37f0a
Alan Nguyen <tuan8101@gmail.com>
2020-07-23 23:13:55 -0600

    Create Balance

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { Balance } from './components/Balance';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
-      <p></p>
+      <div className="container">
+        <Balance />
+      </div>
     </div>
   );
 }
