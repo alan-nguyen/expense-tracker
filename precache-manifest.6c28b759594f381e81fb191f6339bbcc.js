d81f0d81ee2c15faceac6c4947534f9f7da585fa
Alan Nguyen <tuan8101@gmail.com>
2020-07-24 11:07:01 -0600

    Updates

---
+++ precache-manifest.6c28b759594f381e81fb191f6339bbcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c81048c900b37f97426bbcb6588c7d",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "f263dc154859263ef72c",
+    "url": "/expense-tracker/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f263dc154859263ef72c",
+    "url": "/expense-tracker/static/js/main.2a502d93.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
