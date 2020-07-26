e9dfbfd806ded92f8bc09ec5c795a45d0e49d101
Alan Nguyen <tuan8101@gmail.com>
2020-07-26 00:30:45 -0600

    Fix bugs

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const { getTransactions } = require('../controllers/transactions');
 const router = express.Router();
-cosnt { getTransactions } = require('../controllers/transactions');
+const { getTransactions } = require('../controllers/transactions');
 
-router
-  .get('/')
-  .get(getTransactions);
+router.route('/').get(getTransactions);
 
 module.exports = router;
